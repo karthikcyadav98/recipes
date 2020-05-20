7cfd71370201b8c9eeb172523062b9a7bc91036d
karthikcyadav98 <karthikcy98@gmail.com>
2020-05-20 21:09:59 +0530

    database added

---
+++ backend/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://admin:admin@recipes-4apgr.mongodb.net/test?retryWrites=true&w=majority',
+	secretKey: 'key'
+};
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+//mongoDB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongoDB using mongoose
+mongoose
+	.connect(db)
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log('Connection Failure', err));
+
+//Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	//Set Static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+// "process.env.PORT" is used to run the app in heroku
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server running on the port', port));
